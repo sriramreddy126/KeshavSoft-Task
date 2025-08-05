0224f4c11535ba1bf727c6800a4efdb97f962ac5
sriramreddy126 <abbusriramreddy2@gmail.com>
2025-08-05 20:16:23 +0530

    Add files via upload

---
+++ Task- 1/Assets/contact.js
@@ -0,0 +1,17 @@
+ // Simple client-side validation activation
+        (function () {
+            'use strict';
+            const forms = document.querySelectorAll('.needs-validation');
+            Array.from(forms).forEach(form => {
+                form.addEventListener('submit', event => {
+                    if (!form.checkValidity()) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        })();
+
+        // Footer year
+        document.getElementById('year').textContent = new Date().getFullYear();
